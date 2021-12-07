2cf0a2790c1c83fbdad750ed2d879651e7fd5509
Leonardo Ramos <leorrc96@gmail.com>
2021-12-07 17:41:13 -0300

    update(cartContext): se modifico estructura de datos para incluir el total

--- src/cartContext.jsx
+++ src/cartContext.jsx
@@ -1,21 +1,25 @@
 import { createContext, useState } from "react";
+import { calcPriceWithDiscount } from "./helpers/products.api";
 
 export const cartContext = createContext();
 
+const INITIAL_STATE = { total: 0, items: [] };
+
 export const CartProvider = (props) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(INITIAL_STATE);
 
   const addNewItem = (newItem, quantity) => {
     if (!quantity) {
       return;
-    };
-    
+    }
+
     const cartItem = getCartItem(newItem.id);
 
     // si el producto no esta en el carrito, agregrarlo
     if (!cartItem) {
       const temp = cartItems;
-      temp.push({ item: newItem, quantity });
+      temp.items.push({ item: newItem, quantity });
+      temp.total += (calcPriceWithDiscount(newItem) * quantity);
       setCartItems(temp);
       return;
     }
@@ -24,26 +28,45 @@ export const CartProvider = (props) => {
     // solo si la cantidad total NO excede el stock
     const totalQuantity = cartItem.quantity + quantity;
 
-    if (totalQuantity <= cartItem.stock) {
+    if (totalQuantity <= cartItem.item.stock) {
+      const total = cartItems.total + calcPriceWithDiscount(cartItem.item);
       cartItem.quantity = totalQuantity;
+      const newState = {
+        ...cartItems,
+        total,
+      };
+      setCartItems(newState);
     }
   };
 
   const getCartItem = (id) => {
-    return cartItems.find((cartItem) => cartItem.item.id === id);
+    return cartItems.items.find((cartItem) => cartItem.item.id === id);
   };
 
   const removeItem = (id) => {
-    const newItems = cartItems.filter(cartItem => cartItem.item.id !== id);
-    setCartItems(newItems);
-  }
+    let targetItem;
+    const newItems = cartItems.items.filter((cartItem) => {
+      if (cartItem.item.id !== id) {
+        targetItem = cartItem;
+        return true;
+      }
+      return false;
+    });
+    setCartItems({
+      ...cartItems,
+      items: newItems,
+      total: cartItems.total - targetItem.item.price,
+    });
+  };
 
   const clearCartItems = () => {
-    setCartItems([]);
-  }
+    setCartItems({ total: 0, items: [] });
+  };
 
   return (
-    <cartContext.Provider value={{ cartItems, addNewItem, removeItem, clearCartItems }}>
+    <cartContext.Provider
+      value={{ cartItems, addNewItem, removeItem, clearCartItems }}
+    >
       {props.children}
     </cartContext.Provider>
   );
