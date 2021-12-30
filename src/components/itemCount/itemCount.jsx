54ad2b1b4f03988a1cb196eb0587025d9be8d142
Leonardo Ramos <leorrc96@gmail.com>
2021-12-30 19:20:02 -0300

    update(itemCount): cambio de posicion de botones de aumento y disminucion

--- src/components/itemCount/itemCount.jsx
+++ src/components/itemCount/itemCount.jsx
@@ -26,17 +26,18 @@ const ItemCount = ({ stock, onAdd, onRemove, initialCount }) => {
   return (
     <div className="item-count-wrapper">
       <div className="item-count">
-        <button className="item-count__btn" onClick={increment}>
-          <img height="12" src={AddIcon} alt="Incrementar" />
-        </button>
-        <span className="item-count__quantity">{count}</span>
-        <button className="item-count__btn" onClick={decrement}>
+      <button className="item-count__btn" onClick={decrement}>
           <img
             height="13"
             src={SubtractIcon}
             alt="disminuir"
           />
         </button>
+        <span className="item-count__quantity">{count}</span>
+        <button className="item-count__btn" onClick={increment}>
+          <img height="12" src={AddIcon} alt="Incrementar" />
+        </button>
+        
       </div>
       {/* <button className="add-cart-btn" onClick={addToCart}>Agregar al carrito</button> */}
     </div>
