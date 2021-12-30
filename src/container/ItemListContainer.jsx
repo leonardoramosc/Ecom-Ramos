35ca0b8e99a1df52e1a49e8df3409f3443a88d61
Leonardo Ramos <leorrc96@gmail.com>
2021-12-29 22:11:48 -0300

    update: se agregaron spinners

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -2,39 +2,52 @@ import { collection, getDocs, query, where } from "@firebase/firestore";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router";
+import CustomSpinner from "../components/custom-spinner/custom-spinner";
 import ItemList from "../components/item-list/itemList";
 import { db } from "../firebase/firebase";
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([]);
+  const [showSpinner, setShowSpinner] = useState(false);
   const { id: category } = useParams();
 
   useEffect(() => {
+    setShowSpinner(true);
     if (category) {
       const q = query(
         collection(db, "products"),
         where("category", "==", category)
       );
-      getDocs(q).then((querySnapshot) => {
-        const result = querySnapshot.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id };
+      getDocs(q)
+        .then((querySnapshot) => {
+          const result = querySnapshot.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id };
+          });
+          setProducts(result);
+        })
+        .finally(() => {
+          setShowSpinner(false);
         });
-        setProducts(result);
-      });
       return;
     }
 
     const productsCol = collection(db, "products");
-    getDocs(productsCol).then((productList) => {
-      const products = productList.docs.map((doc) => {
-        return { ...doc.data(), id: doc.id };
-      });
-      setProducts(products);
-    });
+      getDocs(productsCol)
+        .then((productList) => {
+          const products = productList.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id };
+          });
+          setProducts(products);
+        })
+        .finally(() => {
+          setShowSpinner(false);
+        });
+
   }, [category]);
 
   return (
     <div>
+      {showSpinner && <CustomSpinner />}
       <ItemList items={products} />
     </div>
   );
--- src/container/itemDetailContainer.jsx
+++ src/container/itemDetailContainer.jsx
@@ -1,14 +1,17 @@
 import { getDoc, doc } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import CustomSpinner from "../components/custom-spinner/custom-spinner";
 import ItemDetail from "../components/itemDetail/itemDetail";
 import { db } from "../firebase/firebase";
 
 const ItemDetailContainer = () => {
   const [item, setItem] = useState(null);
+  const [showSpinner, setShowSpinner] = useState(false);
   const { idProducto } = useParams();
 
   useEffect(() => {
+    setShowSpinner(true);
     const docRef = doc(db, "products", idProducto);
     getDoc(docRef).then(docSnap => {
       if (docSnap.exists()) {
@@ -16,11 +19,14 @@ const ItemDetailContainer = () => {
       } else {
         console.log("No such document!");
       }
-    });
+    }).finally(() => {
+      setShowSpinner(false);
+    })
   }, [idProducto]);
 
   return (
     <div style={{ padding: "30px 0" }}>
+      {showSpinner && <CustomSpinner />}
       {item ? <ItemDetail item={item} /> : null}
     </div>
   );
